a41313f577fe0c579bedf43f04310e934f572a68
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-11-22 12:55:34 -0500

    namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
+namespace :api do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   post '/signup', to: 'users#create'
   get "/me", to: "users#show"
-
   resources :events do
     resources :attendances, only: [:create, :update, :destroy]
   end
+end
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
